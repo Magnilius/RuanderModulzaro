7b55f49f6c6b046f9ecff6ffc4280cf961a8917a
NAGY Dániel László (BKK) <danagy@bkk.hu>
2021-04-14 18:02:37 +0200

    1. feladat kész

---
+++ ModulzaroRuander/Balkezes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulzaroRuander
+{
+    class Balkezes
+    {
+        public string nev { get; set; }
+        public string elso { get; set; }
+        public string utolso { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+
+        public Balkezes(string sor)
+        {
+            string[] s = sor.Split(';');
+            nev = s[0];
+            elso = s[1];
+            utolso = s[2];
+            suly = int.Parse(s[3]);
+            magassag = int.Parse(s[4]);
+        }
+    }
+}
---
+++ ModulzaroRuander/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulzaroRuander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //file beolvasása
+            string[] sorok = File.ReadAllLines("balkezesek.csv");
+            List<Balkezes> balkezesek = new List<Balkezes>();
+
+            foreach (string sor in sorok.Skip(1))
+            {
+                balkezesek.Add(new Balkezes(sor));
+            }
+
+            //1. feladat
+            int N = balkezesek.Count();
+            Console.WriteLine($"1. Feladat: A balkezesek száma {N}");
+
+            //2. feladat
+            int eloszor = 0;
+
+
+            Console.Read();
+        }
+    }
+}
